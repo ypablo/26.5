ae9540205859095e16081fdd3dde296afc846cb9
ypablo <yfedora@gmail.com>
2019-06-11 16:24:56 +0100

    26.5 Chat

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const UsersService = require('./UsersService');
+
+const usersService = new UsersService();
 
 const app = express();
 const server = http.createServer(app);
@@ -13,22 +16,54 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  //miejsce dla funkcji, które zostaną wykonane po podłączeniu klienta
-  // klient nasłuchuje na wiadomość wejścia do czatu
     socket.on('join', (name) => {
-        // użytkownika, który pojawił się w aplikacji, zapisujemy do serwisu trzymającego listę osób w czacie
         usersService.addUser({
         id: socket.id,
         name
         });
-        // aplikacja emituje zdarzenie update, które aktualizuje informację na temat listy użytkowników każdemu nasłuchującemu na wydarzenie 'update'
         io.emit('update', {
         users: usersService.getAllUsers()
         });
     });
+    socket.on('disconnect', () => {
+      usersService.removeUser(socket.id);
+      socket.broadcast.emit('update', {
+        users: usersService.getAllUsers()
+      });
+    });
+    socket.on('message', (message) => {
+      const {name} = usersService.getUserById(socket.id);
+      socket.broadcast.emit('message', {
+        text: message.text,
+        from: name
+      });
+    });
 });
 
+/*
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    usersService.removeUser(socket.id);
+    socket.broadcast.emit('update', {
+      users: usersService.getAllUsers()
+    });
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', (message) => {
+    const {name} = usersService.getUserById(socket.id);
+    socket.broadcast.emit('message', {
+      text: message.text,
+      from: name
+    });
+  });
+});
+*/
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+
+module.exports = (env) => {
+    return {
+        mode: env.NODE_ENV,
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.bundle.js'
+        },
+        plugins: [new HtmlWebpackPlugin({
+            template: 'client/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }), new OptimizeJsPlugin({
+            sourceMap: false
+        })],
+        entry: './client/index.js',
+        output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'app.bundle.js'
+        },
+        devServer: {
+            proxy: {
+                '/socket.io': {
+                    target: 'http://localhost:3000',
+                    ws: true
+                }
+            }
+          }
+
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader",
+                    options: {
+                        plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: 'style-loader' },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+};    
